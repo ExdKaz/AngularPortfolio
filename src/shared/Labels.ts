be34b72e5297b5fef7ad30005dd2b38832878707
abhishek rana <mrabhishekrana123@gmail.com>
2024-11-29 11:18:36 +0530

    project section added

--- src/shared/Labels.ts
+++ src/shared/Labels.ts
@@ -11,5 +11,14 @@ export const LABELS = {
         GETINTOUCH: "Get In Touch",
         BROWSEPROJECTS: "Browse Projects",
         IMAGESRC: "../../assets/images/me.jpg"
+    },
+    projects: {
+        TITLE: 'Projects',
+        WEATHERSRC: '../../assets/images/weather.png',
+        WEATHERTITLE: 'Weather App',
+        WEATHERINFO: 'A weather app made using Angular 15, With real time data from Rapid Api.',
+        SPOTIFYSRC: '../../assets/images/dash.png',
+        SPOTIFYTITLE: 'Spotify Clone',
+        SPOTIFYINFO: 'A Spotify Clone made using Angular 17, With real time data from Spotify Developers Api.'
     }
 }
